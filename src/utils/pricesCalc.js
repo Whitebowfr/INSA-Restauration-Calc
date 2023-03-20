cef03883f2b513badd916664fdd8d1178e8cb20a
Whitebow <whitebowfr@gmail.com>
2023-03-20 10:42:40 +0100

    Fixed bug causing price errors

--- src/utils/pricesCalc.js
+++ src/utils/pricesCalc.js
@@ -1,10 +1,10 @@
 export function getDejPrice(menu) {
     switch (menu) {
-        case '5/7':
+        case '5':
             return 2.14
-        case '5/7*':
+        case '5*':
             return 2.25
-        case '7/7':
+        case '7':
             return 1.87
         default:
             return 2.43
@@ -13,11 +13,11 @@ export function getDejPrice(menu) {
 
 export function getMealPrice(menu) {
     switch (menu) {
-        case '5/7':
+        case '5':
             return 4.23
-        case '5/7*':
+        case '5*':
             return 4.47
-        case '7/7':
+        case '7':
             return 3.74
         default:
             return 4.81
